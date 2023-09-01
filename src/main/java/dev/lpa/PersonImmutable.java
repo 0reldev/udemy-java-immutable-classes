7a32fc1e5fd1464e661813935a6b11d563556422
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-09-01 11:53:18 +0200

    Another lesson about immutability.

---
+++ src/main/java/dev/lpa/PersonImmutable.java
@@ -0,0 +1,50 @@
+package dev.lpa;
+
+import java.util.Arrays;
+
+public class PersonImmutable {
+
+    private final String name;
+    private final String dob;
+    protected final PersonImmutable[] kids;
+
+    public PersonImmutable(String name, String dob, PersonImmutable[] kids) {
+        this.name = name;
+        this.dob = dob;
+        this.kids = kids == null ? null : Arrays.copyOf(kids, kids.length); // Defensive strategy to force immutability.
+    }
+
+    public PersonImmutable(String name, String dob) {
+        this(name, dob, null);
+    }
+
+    protected PersonImmutable(PersonImmutable person) {
+        this(person.getName(), person.getDob(), person.getKids());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    /* This is a defensive strategy to force immutability.
+    * Make this final is the key.*/
+    public final PersonImmutable[] getKids() {
+        return kids == null ? null : Arrays.copyOf(kids, kids.length);
+    }
+
+    @Override
+    public String toString() {
+
+        String kidString = "n/a";
+        if (kids != null) {
+            String[] names = new String[kids.length];
+            Arrays.setAll(names, i -> names[i] = kids[i] == null ? "" : kids[i].name);
+            kidString = String.join(", ", names);
+        }
+        return name + ", dob = " + getDob() + ", kids = " + kidString;
+    }
+}
